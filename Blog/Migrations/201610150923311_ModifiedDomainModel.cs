beea27fd06241a448717e8b784a5a23286e8f684
lengsokpheakdey <sokpheakdeyleng@gmail.com>
2016-10-15 18:10:44 +0700

    ModifieMenuAndCustomizeLoginPage

--- Blog/Migrations/201610150923311_ModifiedDomainModel.cs
+++ Blog/Migrations/201610150923311_ModifiedDomainModel.cs
@@ -2,7 +2,7 @@ namespace Blog.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class ModifiedDomainModel : DbMigration
     {
         public override void Up()
@@ -10,7 +10,7 @@ namespace Blog.Migrations
             AlterColumn("dbo.PostStatus", "Status", c => c.String(nullable: false, maxLength: 50));
             AlterColumn("dbo.PostStatus", "Remark", c => c.String(nullable: false, maxLength: 50));
         }
-        
+
         public override void Down()
         {
             AlterColumn("dbo.PostStatus", "Remark", c => c.String(maxLength: 50));
---
+++ Blog/Models/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Models
+{
+    public class MenuController : Controller
+    {
+        ApplicationDbContext myDbContext = new ApplicationDbContext();
+        // GET: Menu
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            var Categories = myDbContext.Categories.ToList();
+            return View("~/Views/Shared/_Layout.cshtml", Categories);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            myDbContext.Dispose();
+        }
+    }
+}
